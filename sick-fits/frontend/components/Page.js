f9f26612c8da3263a4e5f525b88f60eea5a4d623
Colin Thornton <colinthorntontx@gmail.com>
2020-06-30 15:53:56 +0900

    7. Global Styling and Typography with Styled Components

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import styled, { ThemeProvider } from "styled-components";
+import styled, { ThemeProvider, injectGlobal } from "styled-components";
 
 import Header from "./Header";
 import Meta from "./Meta";
@@ -23,6 +23,38 @@ const Inner = styled.div`
   max-width: ${({ theme }) => theme.maxWidth};
   margin: 0 auto;
   padding: 2rem;
+`;
+
+injectGlobal`
+  @font-face {
+    font-family: 'radnika-next';
+    src: url('/static/radnikanext-medium-webfont.woff2');
+    format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnika-next'
+  }
+
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
 `;
 
 class Page extends Component {
