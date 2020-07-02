c509e0f7568288e942196fb295df6bcab7c06f06
Colin Thornton <colinthorntontx@gmail.com>
2020-07-02 18:23:29 +0900

    19. Updating Items with Queries and Mutations

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -4,6 +4,17 @@ const Mutations = {
     const item = await ctx.db.mutation.createItem({ data: { ...args } }, info);
     return item;
   },
+  async updateItem(parent, args, ctx, info) {
+    const updates = { ...args };
+    delete updates.id;
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: { id: args.id },
+      },
+      info
+    );
+  },
 };
 
 module.exports = Mutations;
